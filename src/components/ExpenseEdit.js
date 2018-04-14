2cf995941eca84e4231043b42a2d51500f79f296
Narath Chiev <narath.chiev@gmail.com>
2018-04-14 11:09:24 -0400

    Moved delete button from ExpenseListItem component to ExpenseEdit

--- src/components/ExpenseEdit.js
+++ src/components/ExpenseEdit.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import ExpenseForm from './ExpenseForm'
-import { editExpense } from '../actions/expenses'
+import { editExpense, removeExpense } from '../actions/expenses'
 
 const ExpenseEdit = (props) =>{
-    console.log(props);
+    const handleDeleteExpense = () => {
+        props.dispatch(removeExpense({ id : props.expense.id }));
+        props.history.push('/');
+    };
+
     return (
-       <ExpenseForm 
-         expense={props.expense} 
-         onSubmit={(expense) => {
-          props.dispatch(editExpense( props.expense.id, expense ));
-          props.history.push('/');
-        }}/>
+        <div>
+            <ExpenseForm 
+             expense={props.expense} 
+             onSubmit={(expense) => {
+                props.dispatch(editExpense( props.expense.id, expense ));
+                props.history.push('/');
+            }}/>
+            <button onClick={ handleDeleteExpense }>Remove</button>
+        </div>
     );
 };
 
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import { Link } from "react-router-dom";
 
 import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => {
-    const handleDeleteExpense = (id ,dispatch) => {
-        dispatch(removeExpense({ id }));
-    };
-
     return (
         <div>
         <Link to={`/edit/${id}`}>
             <h3>{description}</h3>
         </Link>
         <p>{amount} - {createdAt}</p>
-        
-        <button onClick={ (e) => 
-            handleDeleteExpense(id,dispatch) 
-        }>Remove</button>
     </div>
     );
 };
 
-export default connect()(ExpenseListItem);;
+export default ExpenseListItem;
