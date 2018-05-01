501dc385c8653834cd5b574ac49bd26b35f73f7c
Narath Chiev <narath.chiev@gmail.com>
2018-05-01 08:32:58 -0400

    Added refactored expense summary, need to update home component and add more test cases

--- src/components/ExpenseHome.js
+++ src/components/ExpenseHome.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList'
+import ExpenseSummary from './ExpenseSummary';
 import ExpenseListFilter from './ExpenseListFilter';
+
 
 const ExpenseHome = () => (
     <div>
         <ExpenseListFilter />
+        <ExpenseSummary />
         <ExpenseList />
     </div>
 );
