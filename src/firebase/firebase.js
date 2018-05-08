6aa1a5d33a006197c7aff7eac8162270e800dcc3
Narath Chiev <narath.chiev@gmail.com>
2018-05-08 03:41:53 -0400

    Added test/dev enivronment vars

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyDbkpn6zqWn2CxEEy1xBW7zPPms4EAoGN4",
-    authDomain: "expensify-6a62c.firebaseapp.com",
-    databaseURL: "https://expensify-6a62c.firebaseio.com",
-    projectId: "expensify-6a62c",
-    storageBucket: "expensify-6a62c.appspot.com",
-    messagingSenderId: "571941257864"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSENGER_SENDER_ID
   }
 
 firebase.initializeApp(config);
